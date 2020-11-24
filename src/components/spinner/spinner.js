375c88ccbf38827d491e9d441077f4ccb16aa36f
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2020-11-24 18:00:09 +0500

    complete 60 lesson

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+<div className="loadingio-spinner-double-ring-nvcyhpnuoc"><div className="ldio-o4ew1rqb9xo">
+  <div></div>
+  <div></div>
+  <div><div></div></div>
+  <div><div></div></div>
+  </div></div>
+    );
+};
+
+export default Spinner;
